9d52cb57bdbb9c89c1efc744f27c497f07c4d524
hosh1dEcho <kaungzanthaw27904@gmail.com>
2024-08-11 02:18:05 +0630

    last push for tdy

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -4,8 +4,8 @@ const Category = ({ total = 0, filteredItems = [] }) => {
   return (
     <div className="border-2 mt-5 border-slate-100 px-4 py-2 rounded-xl">
       <div className="flex justify-between items-center border-b mt-2 border-slate-100 px-4 py-2 ">
-        <p className="text-slate-100 font-bold">Total</p>
-        <p className="text-slate-100 font-bold">{total.toFixed(2)} B</p>
+        <p className="text-slate-100 font-bold text-2xl">Total</p>
+        <p className="text-slate-100 font-bold text-xl">{total.toFixed(2)} B</p>
       </div>
       <ul>
         {filteredItems.map((item) => (
--- src/components/Total.jsx
+++ src/components/Total.jsx
@@ -1,8 +1,8 @@
-const Total = () => {
+const Total = ({ total = 0 }) => {
   return (
     <section className="p-10">
       <h1 className="text-slate-200 text-2xl mb-3 px-4">Total Amount - </h1>
-      <h1 className="text-center text-slate-50 text-4xl ">1500 THB</h1>
+      <h1 className="text-center text-slate-50 text-4xl ">{total} THB</h1>
     </section>
   );
 };
