5af0d26e32e7c3133d4d9a4f014c3bbbb59a6e3c
hosh1dEcho <kaungzanthaw27904@gmail.com>
2024-08-11 18:06:51 +0630

    by kzt

--- src/components/Divided.jsx
+++ src/components/Divided.jsx
@@ -1,13 +1,20 @@
+// Divided.js
+import React from "react";
+import useDivided from "../store/useDivided";
+
 const Divided = () => {
+  const total = useDivided((state) => state.total);
+
   const classes =
-    "text-xl text-slate-100 border border-slate-200 p-4 rounded-2xl";
+    "text-xl text-slate-100 border border-slate-200 p-4 rounded-2xl text-center";
+
   return (
     <div className="px-10 grid grid-cols-2 gap-4">
       <button className={classes}>Divided By</button>
       <input
         type="text"
-        className={`${classes} bg-transparent text-center`}
-        value="3000 THB"
+        className={`${classes} bg-transparent`}
+        value={`${total.toFixed(2)} THB`}
         disabled
       />
     </div>
