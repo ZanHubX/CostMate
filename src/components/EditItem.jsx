e031cb170149fdde61197afa269150bbca55d0db
hosh1dEcho <kaungzanthaw27904@gmail.com>
2024-08-12 13:46:41 +0630

    last

--- src/components/EditItem.jsx
+++ src/components/EditItem.jsx
@@ -16,10 +16,18 @@ const EditItem = () => {
   const [editData, setEditData] = useState(initialData || []); // Fallback to an empty array if initialData is undefined
 
   console.log(editData);
+  const handleDeleteItem= (id) => {
+    setEditData(editData.filter((item) => item.id !== id));
+    localStorage.setItem("data", JSON.stringify(editData.filter((item) => item.id !== id)));
+  }
+  
+  
+
+  
 
   return (
     <div>
-      <ItemComponent newData={editData} />
+      <ItemComponent newData={editData} handleDeleteButton={handleDeleteItem} />
     </div>
   );
 };
